00bcc731501ada289b8e6fa7592540f5bcca5c46
pranavpurankar <pranav.purankar@gmail.com>
2025-01-07 14:34:45 +0530

    Brute force Moving zero to the end

---
+++ Step_3:Problems_Array_[E-M-H]/Lec_1_Easy/7.MovingZeroEnd.js
@@ -0,0 +1,40 @@
+// Move all zeros to the end of the array 
+/* 
+Algortithm:
+1) Iterate through each element of the array
+2) Pick the non-zero numbers and put in the temp array
+3) The array is remaining with the zeros,
+4) Now push back the temp numbers to the original array
+5) Make the remaining places filled with zero
+*/
+
+let arr = [1,0,2,3,2,0,0,4,5,1];
+
+function movingZeroEnd(arr){
+    let temp = [];
+    let n = arr.length;
+    
+    // Step_1
+    for (let i = 0; i < n; i++) {
+        if(arr[i] !== 0){
+            temp.push(arr[i]);
+        }
+    }
+    console.log('After step_1',arr, temp)
+
+    // // Step_2
+    for(let i=0; i < temp.length; i++){
+        arr[i] = temp[i];
+    }
+    console.log('After step_2', arr, temp)
+
+    // // Step_3
+    for(let i = temp.length; i<n; i++){
+        arr[i]=0
+    }
+
+    return arr;
+}
+
+
+console.log(movingZeroEnd(arr));
